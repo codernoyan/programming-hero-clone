dbc45cb800a0821cb8c045b56822338591435f6e
codernoyan <coder.noyan@gmail.com>
2022-09-04 14:29:36 +0600

    project done

--- js/app.js
+++ js/app.js
@@ -7,15 +7,44 @@ const loadCourseData = async () => {
 
 const showMilestones = async () => {
   const milestones = await loadCourseData();
-  console.log(milestones);
+  // console.log(milestones);
 
   const milestoneContainer = document.getElementById('milestones');
   milestones.forEach(milestone => {
-    console.log(milestone);
-    const { name, image } = milestone;
+    // console.log(milestone);
+    const { name, image, _id } = milestone;
     const li = document.createElement('li');
-    li.innerHTML = `<a>${name}</a>`;
+    li.innerHTML = `<a onclick="showModules('${name}')">${name}</a>`;
     milestoneContainer.appendChild(li);
   })
 }
+
+const showModules = async (milestoneName) => {
+  const milestones = await loadCourseData();
+  // console.log(milestones);
+  const matchedMilestone = milestones.filter(milestone => milestone.name.includes(milestoneName));
+  // console.log(matchedMilestone)
+  const milestoneImage = document.getElementById('milestone__image');
+  const moduleDetails = document.getElementById('module__details');
+  moduleDetails.innerHTML = ``;
+  matchedMilestone.forEach(milestone => {
+    console.log(milestone);
+    milestoneImage.innerHTML = `<img class="transition" src=${milestone.image}>`;
+    milestone.modules.forEach(module => {
+      console.log(module.name)
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td><div class="form-control">
+        <label class="label cursor-pointer">
+          <input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+        </label>
+      </div></td>
+        <td>${module.name}</td>
+        `;
+      moduleDetails.appendChild(tr);
+    })
+  })
+}
+
 showMilestones();
+
